fc690004b0f88b16e0265ac0f97f70135e54e854
JiangWeixian <jiangweixian1994@gmail.com>
2022-10-13 23:06:59 +0800

    :sparkles: attach drag

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { useRoutes, BrowserRouter } from 'react-router-dom'
 import routes from '~react-pages'
 
-console.log(routes)
-
 const Routes = () => {
   const elements = useRoutes(routes)
   return elements
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,16 @@
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
-import { Vector3, Vector3Tuple } from 'three'
+import { Vector3 } from 'three'
 import { OrbitControls as OrbitControlsImpl } from 'three/examples/jsm/controls/OrbitControls'
-import { Text, Html, OrbitControls, useBounds, Bounds } from '@react-three/drei'
+import { Html, OrbitControls } from '@react-three/drei'
 import { useSpring } from '@react-spring/three'
-
-type BoxProps = {
-  position: Vector3Tuple
-}
-
-function Box(props: BoxProps) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef<THREE.Mesh>(null)
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((_state, _delta) => (ref.current!.rotation.x += 0.01))
-  // Return the view, these are regular Threejs elements expressed in JSX
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(_event) => click(!clicked)}
-      onPointerOver={(_event) => hover(true)}
-      onPointerOut={(_event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-}
+import { useDrag } from '@use-gesture/react'
 
 const ArticleBlock = () => {
-  const { camera } = useThree()
+  const { camera, controls } = useThree()
+  const orbit: OrbitControlsImpl = controls as OrbitControlsImpl
   let animating = false
-  const orbit = useRef<OrbitControlsImpl>(null)
   const source = useRef<{ focus: THREE.Vector3; camera: THREE.Vector3 }>({
     focus: new THREE.Vector3(),
     camera: new THREE.Vector3(),
@@ -56,61 +29,91 @@ const ArticleBlock = () => {
   function equals(a, b) {
     return Math.abs(a.x - b.x) < eps && Math.abs(a.y - b.y) < eps && Math.abs(a.z - b.z) < eps
   }
-  useFrame(({ camera }, delta) => {
-    if (animating) {
-      damp(source.current.focus, target.current.focus, 6, delta)
-      damp(source.current.camera, target.current.camera, 6, delta)
-      camera.lookAt(source.current.focus)
-      camera.position.set(source.current.camera!.x, camera.position.y, camera.position.z)
-      camera.updateProjectionMatrix()
-      orbit.current?.target.copy(source.current.focus)
-      orbit.current?.update()
-      if (!equals(source.current.focus, target.current.focus)) {
-        return
-      }
-      console.log('animated finish')
-      animating = false
-    }
-  })
+  // useFrame(({ camera }, delta) => {
+  //   if (animating) {
+  //     damp(source.current.focus, target.current.focus, 6, delta)
+  //     damp(source.current.camera, target.current.camera, 6, delta)
+  //     camera.lookAt(source.current.focus)
+  //     camera.position.set(source.current.camera!.x, camera.position.y, camera.position.z)
+  //     camera.updateProjectionMatrix()
+  //     orbit?.target.copy(source.current.focus)
+  //     orbit?.update()
+  //     if (!equals(source.current.focus, target.current.focus)) {
+  //       return
+  //     }
+  //     console.log('animated finish')
+  //     animating = false
+  //   }
+  // })
   const handleClick = () => {
-    // camera.lookAt(1, 1, 5)
-    // console.log(camera.position)
-    // camera.position.x = 0
-    // const { y, z } = camera.position
-    // const x = 1
-    // camera.position.set(x, y, z)
-    // console.log(orbit.current?.target)
-    // camera.lookAt(1, 1, 5)
-    // orbit.current?.target.set(x, 0, 0)
     console.log(camera.position)
     source.current.camera?.copy(camera.position)
-    source.current.focus?.copy(orbit.current!.target)
+    source.current.focus?.copy(orbit!.target)
     animating = true
   }
+
   return (
     <>
-      {/* set enableRotate: true for detail debug */}
-      <OrbitControls ref={orbit as never} enableRotate={false} makeDefault={true} />
       {/* follow orbit controls transform */}
-      <Html transform={true} occlude={true}>
-        <p onClick={handleClick} className="text-4xl cursor-pointer">
-          hello world
+      <Html as="div" transform={true} occlude={true} className="cursor-pointer w-96">
+        <h3 onClick={handleClick} className="mb-2">Lorem, ipsum.</h3>
+        <p className="text-gray-300 text-xs">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sed dolorem natus quas corporis fuga nesciunt incidunt aut enim quis ratione sunt quo neque atque sapiente, eos nostrum! Facere, maxime deserunt.
         </p>
       </Html>
     </>
   )
 }
 
+const AttachDrag = () => {
+  const { controls, size, viewport, camera } = useThree()
+  const [draging, drag] = useState(false)
+  const orbit: OrbitControlsImpl = controls as OrbitControlsImpl
+  const aspect = size.width / viewport.width
+  const [spring, api] = useSpring(() => ({ position: [0, 0, 0] }))
+  const source = useRef<{ focus: THREE.Vector3; camera: THREE.Vector3 }>({
+    focus: new THREE.Vector3(),
+    camera: new THREE.Vector3(),
+  })
+
+  // Set the drag hook and define component movement based on gesture data
+  useDrag(
+    ({ active, offset: [x, y] }) => {
+      drag(active)
+      source.current.camera?.copy(camera.position)
+      source.current.focus?.copy(orbit!.target)
+      api.start({ position: [-x / aspect, y / aspect, 0] })
+    },
+    { target: window },
+  )
+
+  useFrame(({ camera }) => {
+    if (draging) {
+      const [x, y] = spring.position.get()
+      source.current.focus.x = x
+      source.current.focus.y = y
+      source.current.camera.x = x
+      source.current.camera.y = y
+      console.log(source.current)
+      camera.lookAt(source.current.focus)
+      camera.position.set(source.current.camera!.x, camera.position.y, camera.position.z)
+      camera.updateProjectionMatrix()
+      orbit?.target.copy(source.current.focus)
+      orbit?.update()
+    }
+  })
+
+  return <></>
+}
+
 const Home = () => {
   return (
-    <Canvas>
+    <Canvas onDrag={() => console.log('draging')}>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
-      {/* <Text color="black" anchorX="center" anchorY="middle">
-        hello world
-      </Text> */}
+      {/* set enableRotate: true for detail debug */}
+      <OrbitControls enableRotate={false} makeDefault={true} enableZoom={true} />
+      <AttachDrag />
       <ArticleBlock />
     </Canvas>
   )
