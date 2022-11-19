63482af652db03d9b2e90a0221deba3ca6b6cf72
JiangWeixian <jiangweixian1994@gmail.com>
2022-11-19 23:10:53 +0800

    :sparkles: calc pos

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # 2022-review
 
+## types
+
+`reference` & `twitter_share` cards have horizontal layout
 ## todo
 
 - [ ] twitter share 流白空间太多了
--- scripts/unfurl.ts
+++ scripts/unfurl.ts
@@ -4,19 +4,24 @@ import path from 'path'
 import colors from 'picocolors'
 
 import rss from './rss.json'
-import cached from '../src/assets/unfurl_rss.json'
+import cached from '../src/assets/cache_unfurl_rss.json'
 import type { Metadata } from 'unfurl.js/dist/types'
 
 type Item = Record<string, unknown> & {
+  url: string
+  id: number
   type: string
   meta?: Record<string, unknown>
+  pos: number[][]
+  props: {
+    horizontal: true
+    image: true
+  }
 }
 
-const type2size = {
-  twitter_share: [1, 2],
-  iframe: [2, 2],
-}
-
+/**
+ * @description if item card type changed, re-unfurl meta info
+ */
 const isInvalid = (prev: Item, current: Item) => {
   if (!prev?.meta) {
     return true
@@ -27,34 +32,160 @@ const isInvalid = (prev: Item, current: Item) => {
   return false
 }
 
+const defaultType2SizeMap = {
+  twitter_share: [2, 1],
+  iframe: [2, 2],
+  comment: [1, 1],
+  reference: [1, 1],
+  bg: [1, 1],
+  announcement: [1, 1],
+}
+
+const type2size = (item: Item) => {
+  const type = item.type as unknown as keyof typeof defaultType2SizeMap
+  if (type === 'twitter_share') {
+    return item.props?.horizontal ? [1, 2] : defaultType2SizeMap[type]
+  }
+  if (type === 'reference') {
+    return item.props?.image ? [1, 2] : defaultType2SizeMap[type]
+  }
+  return defaultType2SizeMap[type]
+}
+
+const calc = (rows = 2, cols = 6, items: Item[] = []) => {
+  const createRow = (): number[] => new Array(cols).fill(0)
+  const grids: number[][] = []
+  const fill = (rs: number, cs: number, r = 1, c = 1, index = 0) => {
+    for (let i = 0; i < r; i++) {
+      for (let j = 0; j < c; j++) {
+        grids[rs + i][cs + j] = index + 1
+      }
+    }
+  }
+  const validate = (rs: number, cs: number, r = 1, c = 1) => {
+    const subgrids: number[] = []
+    for (let i = 0; i < r; i++) {
+      for (let j = 0; j < c; j++) {
+        subgrids.push(grids[rs + i][cs + j])
+      }
+    }
+    return subgrids.every((v) => v === 0)
+  }
+  const isFullFill = (row: number[] = []) => {
+    return row.every((v) => v !== 0)
+  }
+  // init grid
+  for (let i = 0; i < rows; i++) {
+    grids.push(createRow())
+  }
+  // search valid grid from pos [r, c]
+  const pos = {
+    r: 0,
+    c: 0,
+  }
+  for (const [index, item] of items.entries()) {
+    const [rs, cs] = type2size(item)
+    let i = pos.r
+    let j = pos.c
+    let found = false
+    console.log(`${colors.bgBlue(colors.black(`[${item.week}-${item.id}] calcing: `))} ${item.url}`)
+    while (!found && i < grids.length && j < grids[0].length) {
+      // start from grid[i][j] = 0
+      while (grids[i][j] !== 0 && j < grids[0].length) {
+        // console.log(item.id, i, j)
+        j += 1
+      }
+      if (rs * cs === 1) {
+        if (j < grids[0].length && validate(i, j, 1, 1)) {
+          fill(i, j, 1, 1, index)
+          // console.log(item.id, grids)
+          // pos.c += 2
+          found = true
+        } else {
+          j += 1
+        }
+      } else if (rs * cs === 4) {
+        if (i < grids.length - 1 && j < grids[0].length - 1 && validate(i, j, 2, 2)) {
+          fill(i, j, 2, 2, index)
+          // console.log(item.id, grids)
+          // pos.c += 2
+          found = true
+        } else {
+          j += 1
+        }
+      } else if (rs === 2) {
+        if (i < grids.length - 1 && validate(i, j, 2)) {
+          fill(i, j, 2, 1, index)
+          // console.log(item.id, grids)
+          // pos.c += 1
+          found = true
+        } else {
+          j += 1
+        }
+      } else if (cs === 2) {
+        if (j < grids[0].length - 1 && validate(i, j, 1, 2)) {
+          fill(i, j, 1, 2, index)
+          // console.log(item.id, grids)
+          // pos.c += 2
+          found = true
+        } else {
+          j += 1
+        }
+      }
+      if (found) {
+        console.log(
+          `${colors.bgGreen(colors.black(`[${item.week}-${item.id}] calc: `))} ${item.url}`,
+        )
+        item.pos = [
+          [i + 1, i + 1 + rs],
+          [j + 1, j + 1 + cs],
+        ]
+      }
+      if (isFullFill(grids[i])) {
+        grids.push(createRow())
+        pos.r += 1
+        pos.c = 0
+        i = pos.r
+        j = pos.c
+      } else if (i >= grids.length || j >= grids[0].length) {
+        grids.push(createRow())
+        i += 1
+        j = 0
+      }
+    }
+  }
+}
+
 const main = async () => {
   const unfurlRSS: Item[] = []
-  for (const item of rss) {
+  const _rss = rss.slice() as unknown as Item[]
+  for (const [id, item] of _rss.entries()) {
+    item.id = id + 1
     const cachedItem = cached.find((i) => i.url === item.url) as unknown as Item
     let result = cachedItem?.meta as unknown as Metadata
+    // validte cache
     if (isInvalid(cachedItem, item)) {
-      console.log(`${colors.bgBlue(colors.black(` unfurling: `))} ${item.url}`)
+      console.log(`${colors.bgBlue(colors.black(`[${item.week}] unfurling: `))} ${item.url}`)
       try {
         result = await unfurl(item.url)
       } catch (e) {
-        console.log(`${colors.bgRed(colors.black(` unfurling failed: `))} ${item.url}`)
+        console.log(
+          `${colors.bgRed(colors.black(`[${item.week}] unfurling failed: `))} ${item.url}`,
+        )
       }
     } else {
-      console.log(`${colors.bgGreen(colors.black(` unfurling: `))} ${item.url}`)
+      console.log(`${colors.bgGreen(colors.black(`[${item.week}] unfurling: `))} ${item.url}`)
     }
-    const unfurlItem: Item = Object.assign({}, item)
+    const unfurlItem = Object.assign({}, item)
     result = result ?? {}
     unfurlItem.meta = result
     unfurlItem.meta.title =
       item.title ?? result.title ?? (result.twitter_card?.title || result.open_graph?.title)
     unfurlItem.meta.cover =
       result.twitter_card?.images?.[0]?.url || result.open_graph?.images?.[0].url
-    // TODO: calucate pos based on item.size
-    if (type2size[item.type]) {
-      unfurlItem.size = type2size[item.type]
-    }
     unfurlRSS.push(unfurlItem)
   }
+  calc(2, 6, unfurlRSS)
   fse.writeJSONSync(path.resolve(__dirname, '../src/assets/unfurl_rss.json'), unfurlRSS, {
     spaces: 2,
   })
